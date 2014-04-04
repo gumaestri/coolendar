7d61dce3efc5de02234edd7c2190129554d2725a
gumaestri <gumaestri@gmail.com>
2014-04-04 08:53:14 -0300

    Creating iPhone project for coolendar

---
+++ coolendar/coolendarTests/coolendarTests.m
@@ -0,0 +1,34 @@
+//
+//  coolendarTests.m
+//  coolendarTests
+//
+//  Created by Gustavo Andrade Maestri on 4/4/14.
+//  Copyright (c) 2014 Aerochimps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface coolendarTests : XCTestCase
+
+@end
+
+@implementation coolendarTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
